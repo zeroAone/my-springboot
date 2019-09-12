70aa81fa1132dda35861834c189de3bb462dff07
dongwx <dongwx@mogulinker.com>
2019-09-12 18:00:03 +0800

    集成lombok

--- day01/src/main/java/com/zeroaone/day01/bo/User.java
+++ day01/src/main/java/com/zeroaone/day01/bo/User.java
@@ -1,9 +1,17 @@
 package com.zeroaone.day01.bo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @Description
  * @auther movw
  * @create 2019-09-12 17:49
  */
+@Getter
+@Setter
 public class User {
+    private long id;
+    private String name;
+    private String email;
 }
